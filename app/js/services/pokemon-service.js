41b93003cb774901c3deaad0502a84941e9b8462
c1281854 <rdrosario@latam.stefanini.com>
2018-10-16 00:54:40 -0300

    Ajustes no servico de PokemonAPI

--- app/js/services/pokemon-service.js
+++ app/js/services/pokemon-service.js
@@ -1,13 +1,13 @@
 angular.module('PokemonModule')
     .factory('PokemonService', PokemonService);
 
-PokemonService.$inject = ['$rootScope'];
+PokemonService.$inject = ['$rootScope', '$http', 'Mensagens', 'TipoMensagem'];
 
-function PokemonService($rootScope) {
+function PokemonService($rootScope, $http, Mensagens, TipoMensagem) {
     var _pokemons = [
-        {id: 1, nome: 'Bulbassaur', tipo: 3, level: 10},
+        /*{id: 1, nome: 'Bulbassaur', tipo: 3, level: 10},
         {id: 2, nome: 'Squirtle', tipo: 2, level: 10},
-        {id: 3, nome: 'Charmander', tipo: 1, level: 10}
+        {id: 3, nome: 'Charmander', tipo: 1, level: 10}*/
     ];
 
     var _tipos = [
@@ -18,21 +18,14 @@ function PokemonService($rootScope) {
 
     var _pokemonSelecionado;
 
-    $rootScope.$on('CADASTRAR_POKEMON', function () {
-        console.log('Factory.$rootScope.emit');
-    });
-
-    $rootScope.$on('CADASTRAR_POKEMON2', function () {
-        console.log('Factory.$rootScope.broadcast');
-    });
-
-    $rootScope.$on('CADASTRAR_POKEMON3', function () {
-        console.log('Factory.$scope.broadcast');
-    });
+    var _listar = function() {
+        return $http.get('http://pokemon.com/rest/pokemon');
+    };
 
     return {
         pokemons: _pokemons,
         tipos: _tipos,
-        pokemonSelecionado: _pokemonSelecionado
+        pokemonSelecionado: _pokemonSelecionado,
+        listar: _listar
     };
 }
--- src/com/stefanini/pokemon/api/PokemonApi.java
+++ src/com/stefanini/pokemon/api/PokemonApi.java
@@ -1,29 +1,26 @@
 package com.stefanini.pokemon.api;
 
-import java.util.List;
-
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.google.gson.Gson;
-import com.stefanini.pokemon.dtos.PokemonDTO;
-import com.stefanini.pokemon.parsers.PokemonParserDTO;
-import com.stefanini.pokemon.persistence.PersistenceSingleton;
+import com.stefanini.pokemon.service.PokemonService;
 
 @Path("/pokemon")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PokemonApi {
 
+	@Inject
+	private PokemonService pokemonService;
 			
 	@GET
 	public Response listar() {
-		Gson gson = new Gson();	
-		PersistenceSingleton pers = PersistenceSingleton.getInstance();
-		PokemonParserDTO dtoParser = new PokemonParserDTO();
-		
-		List<PokemonDTO> dtos = dtoParser.toDtoList(pers.getPokemons());
-		
-		return Response.ok(gson.toJson(dtos)).build();
+		return Response.ok(pokemonService.listar()).build();
 	}
 	
 	
